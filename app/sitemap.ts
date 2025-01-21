0e0fc51f97e81b0155e1b7e99ed531570c832085
vssoares <vsss23@hotmail.com>
2025-01-20 22:53:10 -0300

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,3 @@
-import { getCollections, getPages, getProducts } from '@/lib/shopify';
-import { validateEnvironmentVariables } from '@/lib/utils';
 import { MetadataRoute } from 'next';
 
 type Route = {
@@ -14,38 +12,15 @@ const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
 export const dynamic = 'force-dynamic';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  validateEnvironmentVariables();
-
   const routesMap = [''].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString()
   }));
 
-  const collectionsPromise = getCollections().then((collections) =>
-    collections.map((collection) => ({
-      url: `${baseUrl}${collection.path}`,
-      lastModified: collection.updatedAt
-    }))
-  );
-
-  const productsPromise = getProducts({}).then((products) =>
-    products.map((product) => ({
-      url: `${baseUrl}/product/${product.handle}`,
-      lastModified: product.updatedAt
-    }))
-  );
-
-  const pagesPromise = getPages().then((pages) =>
-    pages.map((page) => ({
-      url: `${baseUrl}/${page.handle}`,
-      lastModified: page.updatedAt
-    }))
-  );
 
   let fetchedRoutes: Route[] = [];
 
   try {
-    fetchedRoutes = (await Promise.all([collectionsPromise, productsPromise, pagesPromise])).flat();
   } catch (error) {
     throw JSON.stringify(error, null, 2);
   }
